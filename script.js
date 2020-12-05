ddd4802447be0410afccc8622a2ecdabc2181310
Sheshu <reddysheshasena@gmail.com>
2020-12-06 00:36:57 +0530

    added button functions

--- script.js
+++ script.js
@@ -2,56 +2,77 @@
 
 let leng = document.querySelectorAll(".drum").length;
 for (let i = 0; i < leng; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", clickk);
 
-    function clickk() {
-        let alpha = this.innerText;
-        switch (alpha) {
-            case 'A':
-                let audio1 = new Audio("/Drum-Set/sounds/tom-1.mp3");
-                audio1.play();
+    document.querySelectorAll(".drum")[i].addEventListener("click", function () {
 
-                break;
-            case 'S':
-                let audio2 = new Audio("/Drum-Set/sounds/tom-2.mp3");
-                audio2.play();
+        let click = this.innerText;
+        makeSound(click);
+        btnPressed(click);
 
-                break;
-            case 'D':
-                let audio3 = new Audio("/Drum-Set/sounds/tom-3.mp3");
-                audio3.play();
+    });
 
-                break;
-            case 'F':
-                let audio4 = new Audio("/Drum-Set/sounds/tom-4.mp3");
-                audio4.play();
+}
 
-                break;
-            case 'G':
-                let audio5 = new Audio("/Drum-Set/sounds/snare.mp3");
-                audio5.play();
+document.addEventListener("keydown", function (event) {
 
-                break;
-            case 'H':
-                let audio6 = new Audio("/Drum-Set/sounds/crash.mp3");
-                audio6.play();
+    let down = event.key;
+    makeSound(down);
+    btnPressed(down);
+});
 
-                break;
-            case 'J':
-                let audio7 = new Audio("/Drum-Set/sounds/kick-bass.mp3");
-                audio7.play();
+function makeSound(par) {
 
-                break;
+    switch (par) {
+        case 'a':
+            let audio1 = new Audio("/Drum-Set/sounds/tom-1.mp3");
+            audio1.play();
 
-            default:
-                console.log("NO AUDIO");
+            break;
+        case 's':
+            let audio2 = new Audio("/Drum-Set/sounds/tom-2.mp3");
+            audio2.play();
 
-                break;
-        }
+            break;
+        case 'd':
+            let audio3 = new Audio("/Drum-Set/sounds/tom-3.mp3");
+            audio3.play();
+
+            break;
+        case 'f':
+            let audio4 = new Audio("/Drum-Set/sounds/tom-4.mp3");
+            audio4.play();
+
+            break;
+        case 'g':
+            let audio5 = new Audio("/Drum-Set/sounds/snare.mp3");
+            audio5.play();
+
+            break;
+        case 'h':
+            let audio6 = new Audio("/Drum-Set/sounds/crash.mp3");
+            audio6.play();
+
+            break;
+        case 'j':
+            let audio7 = new Audio("/Drum-Set/sounds/kick-bass.mp3");
+            audio7.play();
+
+            break;
+
+        default:
+            alert("Press/Click A-S-D-F-G-H-J Buttons only");
+
+            break;
     }
 }
 
+function btnPressed(currentKey){
 
-document.addEventListener("keydown",function(){
-    alert("keyyyy");
-})
+let activeButton=document.querySelector("." + currentKey);
+activeButton.classList.add("pressed")
+
+setTimeout(()=>{
+    activeButton.classList.remove("pressed");
+},100);
+
+}
